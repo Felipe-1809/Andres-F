09ae6622d11b561c3565f64f5047a5de62f01919
Andrés Felipe <andresfelipe15@icloud.com>
2019-09-17 23:46:45 -0500

    Avance 1

--- Tests/AASHTO.py
+++ Tests/AASHTO.py
@@ -1,17 +1,69 @@
 def AASHTO(t34,t38,t4,t10,t50,t100,t200,fondo,ll,lp):
 
-    #Cálculos básicos
-
-    total=t34+t38+t4+t10+t50+t100+t200
-
-    pasa4=100-((100*(t34+t38+t4))/total)
-    pasa200=((100*fondo)/total)
-
+    # Cálculos básicos
+    total = t34+t38+t4+t10+t50+t100+t200
+    pasa4 = 100 - ((100*(t34+t38+t4))/total)
+    pasa200 = ((100*fondo)/total)
     ip = ll-lp
 
-    #Clasificación de grueso o fino
-
+    # Clasificación de grueso o fino
     if pasa200 < 35:
         x = 'suelo fino'
     else:
         x= 'suelo grueso'
+
+    #Definición a
+    if pasa200 < 35:
+        a = 0
+    elif pasa200 > 75:
+        a = 40
+    else:
+        a = pasa200 - 35
+
+    #Definición b
+    if pasa200 < 15 :
+        b = 0
+    elif pasa200 > 45 :
+        b = 40
+    else:
+        a = pasa200 - 15
+
+    #Definición c
+    if ll < 40:
+        c = 0
+    elif ll > 60:
+        c = 20
+    else:
+        c = ll - 35
+
+    #Definición d
+    if ip < 10:
+        d = 0
+    elif ip > 30:
+        d = 20
+    else:
+        d = ip - 10
+
+    #Índice de grupo
+    int(IG) = (0.2*a) + (0.005*a*c) + (0.01*b*d)
+
+    #Clasificación del suelo
+    if x = 'suelo grueso':
+
+        #Verificación que cumpla los parámetros AASHTO
+        try:
+            pasa200 < 36
+        except:
+            print('Los parámetros no cumplen las normas AASHTO')
+            break
+
+        if IG <= 20:
+            #Verificación que cumpla los parámetros AASHTO
+            try:
+                ip < 11
+            except:
+                print('Los parámetros no cumplen las normas AASHTO')
+                break       
+        else:
+
+    else:
--- Tests/USCS.py
+++ Tests/USCS.py
@@ -1,19 +1,15 @@
 def USCS(t34,t38,t4,t10,t50,t100,t200,fondo,ll,lp,d10,d30,d60):
 
-    #Cálculos básicos
-
+    # Cálculos básicos
     total=t34+t38+t4+t10+t50+t100+t200
-
-    pasa4=100-((100*(t34+t38+t4))/total)
-    pasa200=((100*fondo)/total)
-
+    pasa4 = 100 - ((100*(t34+t38+t4))/total)
+    pasa200 = ((100*fondo)/total)
     ip = ll-lp
+    cc = (( d30 * d30 )/( d10 * d60 ))
+    cu = d60 / d10
 
-    cc=(( d30 * d30 )/( d10 * d60 ))
-
-    #Clasificación de grueso o fino
-
-    if pasa200 < 35:
+    # Clasificación de grueso o fino
+    if pasa200 < 50:
         x = 'suelo fino'
     else:
         x= 'suelo grueso'
