f74bd2649c66e06940e3e82b081332ab9972da96
Andrés Felipe <andresfelipe15@icloud.com>
2019-10-08 13:33:59 -0500

    Beginning

---
+++ Soil mechanics/PB.m
@@ -0,0 +1,25 @@
+def PB(q, b)
+
+    %Inicialización de variables
+    contadory = []
+    contadorx = []
+
+    %División de la cuadrícula
+    y = 6 * b %Rango y
+    x = 4 * b %Rango x
+
+    dy = b / 6 %Diferencial y
+    dx = b / 6 %Diferencial x
+
+    canty = y / dy %Cantidad de elementos en y
+    cantx = x / dx %Cantidad de elementos en x
+
+    while contadory <= canty:
+        while contadorx <= cantx:
+            pass
+
+    %Vector de ubicaciones X
+
+    %Vector de ubicaciones Y
+
+    %Cálculo de los esfuerzos en cada punto
